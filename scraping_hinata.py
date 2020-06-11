5255b86a64fe0974cebd4fe29ba5c7d10ffcd0b7
Kona <kitami.kitano20@gmail.com>
2020-06-11 15:00:28 +0900

    みーぱんスクレイピング取得

---
+++ scraping_hinata.py
@@ -0,0 +1,38 @@
+import requests
+import urllib.request
+import os
+from bs4 import BeautifulSoup
+
+
+def scraping():
+    # メンバーURL
+    #member_name = "manatsu.akimoto"
+    # url = "http://blog.nogizaka46.com/" + member_name + "
+    member_name = "mirei_sasaki"
+    url = "https://www.hinatazaka46.com/s/official/diary/member/list?ima=0000&ct=8"
+
+    # フォルダ作成
+    if not os.path.isdir(member_name):  # ”member_name”のフォルダがない場合
+        print("フォルダ作成")
+        os.mkdir(member_name)
+
+    # 保存枚数カウント用
+    cnt = 0
+
+    # BeautifulSoupオブジェクト生成
+    headers = {"User-Agent": "Mozilla/5.0"}
+    soup = BeautifulSoup(requests.get(
+        url, headers=headers).content, 'html.parser')
+
+    # 画像が置かれているhtmlを見つける
+    # for entry in soup.find_all("div", class_="entrybody"):  # 全てのentrybodyを取得
+    for entry in soup.find_all("div", class_="c-blog-article__text"):  # 全てのentrybodyを取得
+        for img in entry.find_all("img"):  # 全てのimgを取得
+            cnt += 1
+            urllib.request.urlretrieve(
+                img.attrs["src"], "./" + member_name + "/" + member_name + "-" + str(cnt) + ".jpeg")
+    print("画像を" + str(cnt) + "枚保存しました。")
+
+
+if __name__ == '__main__':
+    scraping()
